a140782081525017d6e1c723ff5eaf3c7c9eb228
KarlSobischDigitium <karl.sobisch@digitium.de>
2023-02-07 16:14:02 +0100

    Add cloudbuild that builds wheel file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = 'mypackage',
+    version = '1.0.0',
+    author = 'digitium',
+    packages = [
+        'lib',
+    ],
+    install_requires=[]
+)
